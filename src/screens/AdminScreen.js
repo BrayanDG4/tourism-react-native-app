f1341b6b63cefb045bc4ba5d1832bb70a09a08a9
Brayan Gomez <brayandgomez4@gmail.com>
2023-10-24 18:18:20 -0500

    Adding AdminScreen

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,11 +1,112 @@
-import { Text, SafeAreaView, ScrollView } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export const AdminScreen = () => {
+  const users = [
+    { id: 1, name: "Usuario 1" },
+    { id: 2, name: "Usuario 2" },
+    { id: 3, name: "Usuario 3" },
+  ];
   return (
     <SafeAreaView>
-      <ScrollView>
-        <Text>Administrador</Text>
-      </ScrollView>
+      {/* options container */}
+      <View style={{ padding: 10 }}>
+        {/* options */}
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>Panel admnistrador</Text>
+        </View>
+
+        <View style={styles.optionMenu}>
+          <TouchableOpacity style={styles.optionContainer}>
+            <FontAwesome5 name="user-check" size={24} color="white" />
+            <Text style={styles.optionText}>Solicitudes</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={users}
+          keyExtractor={(user) => user.id.toString()}
+          style={{ marginTop: "5%" }}
+          renderItem={({ item }) => (
+            <View style={styles.commerceContainer}>
+              <View style={{ width: "80%" }}>
+                <Text>{item.name}</Text>
+              </View>
+              <View
+                style={{
+                  width: "20%",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <TouchableOpacity style={{ width: "50%" }}>
+                  <FontAwesome name="check-square" size={32} color="green" />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ width: "50%" }}>
+                  <MaterialCommunityIcons
+                    name="alpha-x-box"
+                    size={36}
+                    color="red"
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    backgroundColor: "#0284c7",
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+  },
+  titleText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  optionMenu: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 4,
+    marginTop: "5%",
+  },
+  optionContainer: {
+    backgroundColor: "#0284c7",
+    borderRadius: 50,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "50%",
+  },
+  optionText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+  commerceContainer: {
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 5,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,58 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, Image, View, StyleSheet } from "react-native";
+
+import Animated, { FadeInUp } from "react-native-reanimated";
+
+import waves from "../../assets/commercial-allies/waves-bg.png";
+
+import homeImage from "../../assets/home-screen/home-image.jpeg";
 
 export const HomeScreen = () => {
   return (
     <SafeAreaView>
-        <Text>HOME SCREEN</Text>
+      <Animated.Image
+        style={{ height: "146%", width: "100%", position: "absolute" }}
+        entering={FadeInUp.delay(200).duration(1000).springify()}
+        source={waves}
+      />
+      <Text
+        style={{
+          color: "white",
+          fontWeight: "bold",
+          fontSize: 40,
+          textAlign: "center",
+        }}
+      >
+        AGUACHICA TURÍSTICA
+      </Text>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Animated.Image
+            style={styles.image}
+            entering={FadeInUp.delay(200).duration(1000).springify()}
+            source={homeImage}
+          />
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: "80%",
+    marginTop: "10%",
+    aspectRatio: 1, // Mantiene una relación de aspecto cuadrada
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+});
